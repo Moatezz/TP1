04a333f41ba36c40f0f8ffe2c15f86e65bc12228
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-05 11:53:03 +0100

    added CP top level menu

--- plugins/control-plugin/admin/admin-menu.php
+++ plugins/control-plugin/admin/admin-menu.php
@@ -3,3 +3,15 @@ if (!defined('ABSPATH')) {
 
 exit;
 }
+
+function controlpanel_toplevel_menu() {
+add_menu_page(
+'Control Panel',
+'Controls',
+'manage_options',
+'control-plugin',
+'controlpanel_display_page',
+'dashicons-admin-generic',
+null
+)
+}
