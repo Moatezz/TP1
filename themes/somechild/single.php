a474bd6b63d975644f53464390cd03c3454027bb
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-05 10:42:16 +0100

    work start  5/5 | removing unecessary plugins

--- themes/somechild/single.php
+++ themes/somechild/single.php
@@ -70,12 +70,30 @@
 													</div>
 
 
-													<?php
-													function teting(){
-														require './components/author-description.php'
-													};
-													add_action('after_setup_theme', 'teting');
-													 ?>
+													<div class="post-desc">
+														<div class='post-desc-divider'></div>
+														<div class="post-desc-flex-container">
+															<?php
+															$id = get_the_author_ID();
+															$data = get_user_meta($id);
+															$avatar = get_avatar_url($id);
+
+															//var_dump($test);
+															echo '<img class="post-desc-avatar" src="' . $avatar . '" />'
+															?>
+
+															<?php
+															echo "<div  class='post-desc-name'>" . $data['first_name'][0] . " " .  $data['last_name'][0] . '</div>';
+															?>
+
+														</div>
+														<div class='post-desc-divider'></div>
+														<?php
+														echo "<div  class='post-desc-desc'>" . $data['description'][0] . '</div>';
+														?>
+
+
+													</div>
 												</div>
 											</div>
 											<div class="tags">
