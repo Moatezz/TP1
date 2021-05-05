d7e80b6e0a8a013a340be1f50412f7e18bb136c0
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-05 11:59:06 +0100

    init display-page.php

---
+++ plugins/control-plugin/admin/dispaly-page.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+function controlpanel_display_page()
+{
+    if (!current_user_can('manage_options')) return;
+}
+?>
+<div class="cp-wrapper">
+
+</div>
