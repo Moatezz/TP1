0712070d3158b75fbdbc039cb241b80f5e53d27d
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-07 17:31:45 +0100

    update color field from color to bgcolor

--- plugins/control-plugin/admin/register-settings.php
+++ plugins/control-plugin/admin/register-settings.php
@@ -12,11 +12,19 @@ function controlpanel_register_settings()
         'controlpanel_cb_bnb',
         'control-panel'
     );
-        add_settings_section(
+    add_settings_section(
         'conrolpanel_section_author_desc',
         'Author Description',
         'controlpanel_cb_author_desc',
         'control-panel'
-    ); 
+    );
+    add_settings_field(
+        'bgcolor_bnb',
+        'bgcolor',
+        'control_panel_bnb_color_cb',
+        'controlpanel',
+        'controlpanel_section_bnb)',
+        ['id' => 'bgcolor_bnb', 'label' => 'choose a background color:']
+    );
 }
 add_action('admin_init', 'controlpanel_register_settings');
