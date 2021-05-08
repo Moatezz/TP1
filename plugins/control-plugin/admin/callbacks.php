3c965387d4e0b6b7df31723febef8533e95f76a4
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-08 10:20:53 +0100

    added options to bgcolor cb

--- plugins/control-plugin/admin/callbacks.php
+++ plugins/control-plugin/admin/callbacks.php
@@ -12,7 +12,14 @@ function controlpanel_cb_author_desc()
 {
     echo 'anydhing';
 }
-function control_panel_bnb_bgcolor_cb()
+function control_panel_bnb_bgcolor_cb($args)
 {
-    echo 'something i guess!!';
+    $options = get_option('controlpanel_options', controlpanel_options_default());
+    $id = isset($args['id']) ? $args['id'] : '';
+    $label = isset($args['label']) ? $args['label'] : '';
+    $value = isset($options['id']) ? sanitize_text_field($options[$id]) : '';
+
+    echo '<label for=" controlpanel_options_' . $id . '">' . $label . '</label> <br />';
+    echo '<input id="controlpanel_options_' . $id . '" name="controlpanel_options_[' . $id . ']"
+    type="text" value="' . $value . '">';
 }
