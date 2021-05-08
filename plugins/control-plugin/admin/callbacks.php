5f62eb97d2776e71db0fd2669bfaab0520a089e0
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-08 12:34:58 +0100

    added var dump

--- plugins/control-plugin/admin/callbacks.php
+++ plugins/control-plugin/admin/callbacks.php
@@ -18,11 +18,13 @@ function controlpanel_cb_author_desc()
 function control_panel_bnb_bgcolor_cb($args)
 {
     $options = get_option('controlpanel_options', controlpanel_options_default());
-    $id = isset($args['id']) ? $args['id'] : '';
-    $label = isset($args['label']) ? $args['label'] : '';
-    $value = isset($options['id']) ? sanitize_text_field($options[$id]) : '';
 
-    echo '<label for=" controlpanel_options_' . $id . '">' . $label . '</label> <br />';
-    echo '<input id="controlpanel_options_' . $id . '" name="controlpanel_options_[' . $id . ']"
-    type="text" value="' . $value . '">';
+    $id    = isset($args['id'])    ? $args['id']    : '';
+    $label = isset($args['label']) ? $args['label'] : '';
+
+    $value = isset($options[$id]) ? sanitize_text_field($options[$id]) : '';
+    var_dump($options['bgcolor_bnb']);
+
+    echo '<input id="controlpanel_options_' . $id . '" name="controlpanel_options[' . $id . ']" type="text" size="40" value="' . $value . '"><br />';
+    echo '<label for="controlpanel_options_' . $id . '">' . $label . '</label>';
 }
