fb411b460d712bc151b214129201f20f93545988
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-09 10:58:02 +0100

    added text field cb

--- plugins/control-plugin/admin/callbacks.php
+++ plugins/control-plugin/admin/callbacks.php
@@ -15,7 +15,7 @@ function controlpanel_cb_author_desc()
 {
     echo 'Here you can adjust multiple settings for the Author Description';
 }
-function controlpanel_bnb_color_cb($args)
+function controlpanel_color_cb($args)
 {
     $options = get_option('controlpanel_options', controlpanel_options_default());
 
@@ -27,7 +27,7 @@ function controlpanel_bnb_color_cb($args)
     echo '<input id="controlpanel_options_' . $id . '" name="controlpanel_options[' . $id . ']" type="color" value="' . $value . '"><br />';
     echo '<label for="controlpanel_options_' . $id . '" class="controlpanel-label" style="opacity: 0.5; font-size: smaller">' . $label . '</label>';
 }
-function controlpanel_bnb_range_cb($args)
+function controlpanel_range_cb($args)
 {
 
     $options = get_option('controlpanel_options', controlpanel_options_default());
@@ -38,5 +38,18 @@ function controlpanel_bnb_range_cb($args)
     //var_dump($id);
 
     echo '<input id="controlpanel_options_' . $id . '" name="controlpanel_options[' . $id . ']" type="range" min="10" max="100" value="' . $value . '"><br />';
+    echo '<label for="controlpanel_options_' . $id . '" class="controlpanel-label" style="opacity: 0.5; font-size: smaller">' . $label . '</label>';
+}
+function controlpanel_text_field_cb($args)
+{
+
+    $options = get_option('controlpanel_options', controlpanel_options_default());
+
+    $id    = isset($args['id'])    ? $args['id']    : '';
+    $label = isset($args['label']) ? $args['label'] : '';
+    $value = isset($options[$id]) ? sanitize_text_field($options[$id]) : '';
+    //var_dump($id);
+
+    echo '<input id="controlpanel_options_' . $id . '" name="controlpanel_options[' . $id . ']" type="text" value="' . $value . '"><br />';
     echo '<label for="controlpanel_options_' . $id . '" class="controlpanel-label" style="opacity: 0.5; font-size: smaller">' . $label . '</label>';
 }
